264497b83fcef47199f9fb96a8672bc7c6b43360
Chris John Agarap <chrisjohn.seej@gmail.com>
2018-09-15 02:37:59 +0800

    init

---
+++ src/studentrecord/StudentRecord.java
@@ -0,0 +1,12 @@
+package studentrecord;
+/**
+ *
+ * @author Seej
+ */
+public class StudentRecord {
+    public static void main(String[] args) {
+        studRecordGUI stud_gui= new studRecordGUI();
+        stud_gui.setVisible(true);
+        stud_gui.setResizable(false);
+    }
+}
